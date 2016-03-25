803edd1d2414530dc41505df888f32c5b3bffb94
unknown <001373@tb001373-1.zjzxlh.cn>
2016-03-25 19:27:49 +0800

    tb

---
+++ classes/TBjs.js
@@ -0,0 +1,13 @@
+(function(factory) {
+    if (typeof exports === 'undefined') {
+        factory(define)
+    } else {
+        factory(function(self) {
+            self(require, exports, module);
+        });
+    }
+})(function(define) {
+    define(function(require, exports, module) {
+
+    })
+})

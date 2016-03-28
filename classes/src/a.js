b47ffa3ddd8cba8e6e1260474e824c25ba4d055c
unknown <001373@tb001373-1.zjzxlh.cn>
2016-03-28 19:17:10 +0800

    aaa

--- classes/src/a.js
+++ classes/src/a.js
@@ -1,179 +1,188 @@
-(function(window, undefined) {
-
-    var PENDING = undefined,
-        FULFILLED = 1,
-        REJECTED = 2;
-
-    var isFunction = function(obj) {
-        return 'function' === typeof obj;
-    }
-    var isArray = function(obj) {
-        return Object.prototype.toString.call(obj) === "[object Array]";
-    }
-    var isThenable = function(obj) {
-        return obj && typeof obj['then'] == 'function';
-    }
-
-    var transition = function(status, value) {
-        var promise = this;
-        if (promise._status !== PENDING) return;
-        // 所以的执行都是异步调用，保证then是先执行的
-        setTimeout(function() {
-            promise._status = status;
-            publish.call(promise, value);
+(function(factory) {
+    if (typeof exports === 'undefined') {
+        factory(define)
+    } else {
+        factory(function(self) {
+            self(require, exports, module);
         });
     }
-    var publish = function(val) {
-        var promise = this,
-            fn,
-            st = promise._status === FULFILLED,
-            queue = promise[st ? '_resolves' : '_rejects'];
+})(function(define) {
+    define(function(require, exports, module) {
 
-        while (fn = queue.shift()) {
-            val = fn.call(promise, val) || val;
+        var PENDING = undefined,
+            FULFILLED = 1,
+            REJECTED = 2;
+
+        var isFunction = function(obj) {
+            return 'function' === typeof obj;
         }
-        promise[st ? '_value' : '_reason'] = val;
-        promise['_resolves'] = promise['_rejects'] = undefined;
-    }
-
-    var Promise = function(resolver) {
-        if (!isFunction(resolver))
-            throw new TypeError('You must pass a resolver function as the first argument to the promise constructor');
-        if (!(this instanceof Promise)) return new Promise(resolver);
-
-        var promise = this;
-        promise._value;
-        promise._reason;
-        promise._status = PENDING;
-        promise._resolves = [];
-        promise._rejects = [];
-
-        var resolve = function(value) {
-            transition.apply(promise, [FULFILLED].concat([value]));
+        var isArray = function(obj) {
+            return Object.prototype.toString.call(obj) === "[object Array]";
         }
-        var reject = function(reason) {
-            transition.apply(promise, [REJECTED].concat([reason]));
+        var isThenable = function(obj) {
+            return obj && typeof obj['then'] == 'function';
         }
 
-        resolver(resolve, reject);
-    }
-
-    Promise.prototype.then = function(onFulfilled, onRejected) {
-        var promise = this;
-        // 每次返回一个promise，保证是可thenable的
-        return Promise(function(resolve, reject) {
-            function callback(value) {
-                var ret = isFunction(onFulfilled) && onFulfilled(value) || value;
-                if (isThenable(ret)) {
-                    ret.then(function(value) {
-                        resolve(value);
-                    }, function(reason) {
-                        reject(reason);
-                    });
-                } else {
-                    resolve(ret);
-                }
-            }
-
-            function errback(reason) {
-                reason = isFunction(onRejected) && onRejected(reason) || reason;
-                reject(reason);
-            }
-            if (promise._status === PENDING) {
-                promise._resolves.push(callback);
-                promise._rejects.push(errback);
-            } else if (promise._status === FULFILLED) { // 状态改变后的then操作，立刻执行
-                callback(promise._value);
-            } else if (promise._status === REJECTED) {
-                errback(promise._reason);
-            }
-        });
-    }
-
-    Promise.prototype.catch = function(onRejected) {
-        return this.then(undefined, onRejected)
-    }
-
-    Promise.prototype.delay = function(ms) {
-        return this.then(function(val) {
-            return Promise.delay(ms, val);
-        })
-    }
-
-    Promise.delay = function(ms, val) {
-        return Promise(function(resolve, reject) {
+        var transition = function(status, value) {
+            var promise = this;
+            if (promise._status !== PENDING) return;
+            // 所以的执行都是异步调用，保证then是先执行的
             setTimeout(function() {
-                resolve(val);
-            }, ms);
-        })
-    }
-
-    Promise.resolve = function(arg) {
-        return Promise(function(resolve, reject) {
-            resolve(arg)
-        })
-    }
-
-    Promise.reject = function(arg) {
-        return Promise(function(resolve, reject) {
-            reject(arg)
-        })
-    }
-
-    Promise.all = function(promises) {
-        if (!isArray(promises)) {
-            throw new TypeError('You must pass an array to all.');
+                promise._status = status;
+                publish.call(promise, value);
+            });
         }
-        return Promise(function(resolve, reject) {
-            var i = 0,
-                result = [],
-                len = promises.length;
+        var publish = function(val) {
+            var promise = this,
+                fn,
+                st = promise._status === FULFILLED,
+                queue = promise[st ? '_resolves' : '_rejects'];
 
-            function resolver(index) {
-                return function(value) {
-                    resolveAll(index, value);
-                };
+            while (fn = queue.shift()) {
+                val = fn.call(promise, val) || val;
+            }
+            promise[st ? '_value' : '_reason'] = val;
+            promise['_resolves'] = promise['_rejects'] = undefined;
+        }
+
+        var Promise = function(resolver) {
+            if (!isFunction(resolver))
+                throw new TypeError('You must pass a resolver function as the first argument to the promise constructor');
+            if (!(this instanceof Promise)) return new Promise(resolver);
+
+            var promise = this;
+            promise._value;
+            promise._reason;
+            promise._status = PENDING;
+            promise._resolves = [];
+            promise._rejects = [];
+
+            var resolve = function(value) {
+                transition.apply(promise, [FULFILLED].concat([value]));
+            }
+            var reject = function(reason) {
+                transition.apply(promise, [REJECTED].concat([reason]));
             }
 
-            function rejecter(reason) {
-                reject(reason);
-            }
+            resolver(resolve, reject);
+        }
 
-            function resolveAll(index, value) {
-                result[index] = value;
-                if (index == len - 1) {
-                    resolve(result);
+        Promise.prototype.then = function(onFulfilled, onRejected) {
+            var promise = this;
+            // 每次返回一个promise，保证是可thenable的
+            return Promise(function(resolve, reject) {
+                function callback(value) {
+                    var ret = isFunction(onFulfilled) && onFulfilled(value) || value;
+                    if (isThenable(ret)) {
+                        ret.then(function(value) {
+                            resolve(value);
+                        }, function(reason) {
+                            reject(reason);
+                        });
+                    } else {
+                        resolve(ret);
+                    }
                 }
-            }
 
-            for (; i < len; i++) {
-                promises[i].then(resolver(i), rejecter);
-            }
-        });
-    }
-
-    Promise.race = function(promises) {
-        if (!isArray(promises)) {
-            throw new TypeError('You must pass an array to race.');
+                function errback(reason) {
+                    reason = isFunction(onRejected) && onRejected(reason) || reason;
+                    reject(reason);
+                }
+                if (promise._status === PENDING) {
+                    promise._resolves.push(callback);
+                    promise._rejects.push(errback);
+                } else if (promise._status === FULFILLED) { // 状态改变后的then操作，立刻执行
+                    callback(promise._value);
+                } else if (promise._status === REJECTED) {
+                    errback(promise._reason);
+                }
+            });
         }
-        return Promise(function(resolve, reject) {
-            var i = 0,
-                len = promises.length;
 
-            function resolver(value) {
-                resolve(value);
+        Promise.prototype.catch = function(onRejected) {
+            return this.then(undefined, onRejected)
+        }
+
+        Promise.prototype.delay = function(ms) {
+            return this.then(function(val) {
+                return Promise.delay(ms, val);
+            })
+        }
+
+        Promise.delay = function(ms, val) {
+            return Promise(function(resolve, reject) {
+                setTimeout(function() {
+                    resolve(val);
+                }, ms);
+            })
+        }
+
+        Promise.resolve = function(arg) {
+            return Promise(function(resolve, reject) {
+                resolve(arg)
+            })
+        }
+
+        Promise.reject = function(arg) {
+            return Promise(function(resolve, reject) {
+                reject(arg)
+            })
+        }
+
+        Promise.all = function(promises) {
+            if (!isArray(promises)) {
+                throw new TypeError('You must pass an array to all.');
             }
+            return Promise(function(resolve, reject) {
+                var i = 0,
+                    result = [],
+                    len = promises.length;
 
-            function rejecter(reason) {
-                reject(reason);
+                function resolver(index) {
+                    return function(value) {
+                        resolveAll(index, value);
+                    };
+                }
+
+                function rejecter(reason) {
+                    reject(reason);
+                }
+
+                function resolveAll(index, value) {
+                    result[index] = value;
+                    if (index == len - 1) {
+                        resolve(result);
+                    }
+                }
+
+                for (; i < len; i++) {
+                    promises[i].then(resolver(i), rejecter);
+                }
+            });
+        }
+
+        Promise.race = function(promises) {
+            if (!isArray(promises)) {
+                throw new TypeError('You must pass an array to race.');
             }
+            return Promise(function(resolve, reject) {
+                var i = 0,
+                    len = promises.length;
 
-            for (; i < len; i++) {
-                promises[i].then(resolver, rejecter);
-            }
-        });
-    }
+                function resolver(value) {
+                    resolve(value);
+                }
 
-    window.Promise = Promise;
+                function rejecter(reason) {
+                    reject(reason);
+                }
 
-})(window);
+                for (; i < len; i++) {
+                    promises[i].then(resolver, rejecter);
+                }
+            });
+        }
+
+        module.exports = Promise;
+    })
+})
--- classes/src/promise.js
+++ classes/src/promise.js
@@ -9,74 +9,101 @@
 })(function(define) {
     define(function(require, exports, module) {
         var toolkit = require('./toolkit');
+        var isThenable = function(obj) {
+            return obj && typeof obj['then'] == 'function';
+        };
 
         function Promise(callback) {
             //pending 等待
             //fulfilled 肯定
             //rejected 否定
             //settled 结束
+
+            if (!(this instanceof Promise)) return new Promise(callback);
             var status = 'pending';
-            this.next = null;
             var resolves = [];
             var rejects = [];
-            var _this = this;
-            this.resolve = function(resolve) {
+            var value, reason;
+
+
+            function resolve(data) {
+                if (status !== 'pending') return;
                 status = 'fulfilled';
-                var firstFn;
-                while (firstFn = resolves.shift()) {
-                    var result = firstFn(resolve);
-                    if (_this.next) {
-                        _this.next.resolve(result);
-
+                value = data;
+                while (resolves.length) {
+                    var firstFn = resolves.shift();
+                    if (toolkit.isFunction(firstFn)) {
+                        firstFn(data);
                     }
-                }
-            };
-            this.reject = function(e) {
-                status = 'rejected';
-                var firstFn;
-                while (firstFn = resolves.shift()) {
-                    var result = firstFn(e);
-                    if (_this.next) {
-                        _this.next.reject(result);
-                    }
-                }
-            };
-
-            this.then = function(resolve, reject) {
-                var next = this.next || (this.next = new Promise());
-                switch (status) {
-                    case 'pending':
-                        toolkit.isFunction(resolve) && resolves.push(resolve);
-                        toolkit.isFunction(reject) && rejects.push(reject);
-                        return next;
-                    case 'fulfilled':
-                        if (toolkit.isFunction(resolve)) {
-                            try {
-                                next.resolve(resolve());
-                            } catch (e) {
-                                this.reject(e);
-                            }
-                        } else {
-                            next.resolve(resolve);
-                        }
-                        return next;
-                    case 'rejected':
-                        if (toolkit.isFunction(reject)) {
-                            try {
-                                next.reject(reject());
-                            } catch (e) {
-                                this.reject(e);
-                            }
-                        } else {
-                            next.reject(resolve);
-                        }
-                        return next;
                 }
             }
 
-            toolkit.isFunction(callback) && callback(this.resolve, this.reject);
-        }
 
+            function reject(errorMsg) {
+                if (status !== 'pending') return;
+                status = 'rejected';
+                reason = errorMsg;
+                while (rejects.length) {
+                    var firstFn = rejects.shift();
+                    if (toolkit.isFunction(firstFn)) {
+                        firstFn(errorMsg);
+                    }
+                }
+            }
+
+
+
+
+
+            toolkit.isFunction(callback) && callback(resolve, reject);
+
+            this.then = function(onFulfilled, onRejected) {
+                //then方法必须返回一个promise
+
+                //onFulfilled 当得到一个肯定的结果
+                //onRejected 当得到一个否定的结果
+                return new Promise(function(resolve, reject) {
+                    //这个函数会被立即执行
+
+                    //resolve 通知当前promise得到了一个肯定的结果
+                    //reject 通知当前promise得到了一个否定的结果
+                    function resolveCallback(value) {
+                        var thenable = toolkit.isFunction(onFulfilled) && onFulfilled(value) || value;
+                        if (isThenable(thenable)) {
+                            thenable.then(function(vlaue) {
+                                resolve(value);
+                            }, function(reason) {
+                                reject(reason);
+                            })
+                        } else {
+                            resolve(thenable);
+                        }
+                    }
+
+                    function rejectCallback(reason) {
+                        reason = toolkit.isFunction(onRejected) && onRejected(reason) || reason;
+                        reject(reason);
+                    }
+
+
+                    switch (status) {
+                        case 'pending':
+                            //如果上一个promise还处于挂起状态
+                            resolves.push(resolveCallback);
+                            rejects.push(rejectCallback);
+                            break;
+                        case 'fulfilled':
+                            //如果上一个promise得到一个肯定的结果
+                            //resolveCallback(resolve);
+                            resolveCallback(value);
+                            break;
+                        case 'rejected':
+                            rejectCallback(reason);
+                            break;
+                    }
+                })
+            }
+        }
         module.exports = Promise;
     })
 })

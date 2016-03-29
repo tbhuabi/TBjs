10c18fa6cc91e7a12cd16caa81f0b203e382f5d2
unknown <001373@tb001373-1.zjzxlh.cn>
2016-03-29 14:28:29 +0800

    抄完promise

--- classes/src/promise.js
+++ classes/src/promise.js
@@ -8,101 +8,147 @@
     }
 })(function(define) {
     define(function(require, exports, module) {
-        var toolkit = require('./toolkit');
-        var isThenable = function(obj) {
-            return obj && typeof obj['then'] == 'function';
-        };
+        'use strict'
 
-        function Promise(callback) {
-            //pending 等待
-            //fulfilled 肯定
-            //rejected 否定
-            //settled 结束
+        function Promise(fn) {
 
-            if (!(this instanceof Promise)) return new Promise(callback);
-            var status = 'pending';
-            var resolves = [];
-            var rejects = [];
-            var value, reason;
+            // 如果不是一个Promise对象，那么调用构造函数包装下
+            if (!(this instanceof Promise)) return new Promise(fn);
 
+            // 参数必须是一个函数
+            if (typeof fn !== 'function') throw new TypeError('not a function');
 
-            function resolve(data) {
-                status = 'fulfilled';
-                value = data;
-                setTimeout(function() {
-                    while (resolves.length) {
-                        var firstFn = resolves.shift();
-                        if (toolkit.isFunction(firstFn)) {
-                            firstFn(data);
-                        }
-                    }
-                })
-            }
+            var state = null;
+            var delegating = false;
+            var value = null;
+            var deferreds = [];
+            var self = this;
 
-
-            function reject(errorMsg) {
-                status = 'rejected';
-                reason = errorMsg;
-                setTimeout(function() {
-                    while (rejects.length) {
-                        var firstFn = rejects.shift();
-                        if (toolkit.isFunction(firstFn)) {
-                            firstFn(errorMsg);
-                        }
-                    }
-                })
-            }
-
-
-            toolkit.isFunction(callback) && callback(resolve, reject);
-
+            // 成员函数：then。返回Promise2
             this.then = function(onFulfilled, onRejected) {
-                //then方法必须返回一个promise
-
-                //onFulfilled 当得到一个肯定的结果
-                //onRejected 当得到一个否定的结果
+                // 返回一个新的Promise，Promise2。
                 return new Promise(function(resolve, reject) {
-                    //这个函数会被立即执行
+                    // Handler没特别的，就是为了保存4个参数
+                    handle(new Handler(onFulfilled, onRejected, resolve, reject));
+                })
+            };
 
-                    //resolve 通知当前promise得到了一个肯定的结果
-                    //reject 通知当前promise得到了一个否定的结果
-                    function resolveCallback(value) {
-                        var thenable = toolkit.isFunction(onFulfilled) && onFulfilled(value) || value;
-                        if (isThenable(thenable)) {
-                            thenable.then(function(vlaue) {
-                                resolve(value);
-                            }, function(reason) {
-                                reject(reason);
-                            })
-                        } else {
-                            resolve(thenable);
+            // 处理defer，fulfill, reject等问题
+            //
+            function handle(deferred) {
+                // 还在pending？那么走人
+                if (state === null) {
+                    deferreds.push(deferred);
+                    return;
+                }
+                // 状态已变：
+                setTimeout(function() {
+                    // 根据state来决定是调用fulfill还是reject
+                    var cb = state ? deferred.onFulfilled : deferred.onRejected;
+                    if (cb === null) {
+                        // 假如没有提供对应的callback，那么调用“默认”的回调
+                        (state ? deferred.resolve : deferred.reject)(value);
+                        return;
+                    }
+
+                    // 调用对应的回调
+                    var ret;
+                    try {
+                        ret = cb(value);
+                    } catch (e) {
+                        // 出现异常了，那么把状态改成reject
+                        deferred.reject(e);
+                        return;
+                    }
+
+                    // 回调运行没问题，那么就把回调的返回值作为自己的值，状态变成fulfilled
+                    deferred.resolve(ret);
+                })
+            }
+
+            // 作为第一个参数传给你写的那个函数
+            //
+            function resolve(newValue) {
+                // 这里有点没搞懂，为啥要针对delegating flag来return，
+                // 其实假如delegating标记已设置，这个函数就再也进不来了呀。
+                // 难道仅仅为了防止出错？
+                if (delegating) return;
+                resolve_(newValue);
+            }
+
+            function resolve_(newValue) {
+                // 说明已经被处理过了！不用再处理了，其实如果会被调用两次，应该是出错了!
+                if (state !== null) return;
+                try { //Promise Resolution Procedure: https://github.com/promises-aplus/promises-spec#the-promise-resolution-procedure
+                    if (newValue === self) {
+                        throw new TypeError('A promise cannot be resolved with itself.');
+                    }
+
+                    // resolve的返回值可以是普通值，也可以是一个promise，针对promise要特殊处理下
+                    if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
+
+                        // 判别他是不是一个promise
+                        var then = newValue.then;
+                        if (typeof then === 'function') {
+
+                            // 是的。。那就有点麻烦，设个标记先
+                            delegating = true;
+                            // 根据 Promise/A+，当前promise的状态保持pending，且依赖于返回的promise的状态，即newValue
+                            // 所以调用 newValue.then。
+                            then.call(newValue, resolve_, reject_);
+                            return;
                         }
                     }
 
-                    function rejectCallback(reason) {
-                        reason = toolkit.isFunction(onRejected) && onRejected(reason) || reason;
-                        reject(reason);
-                    }
+                    // 普通值，简单了
+                    state = true;
+                    value = newValue;
+                    // 触发依赖于这个promise的所有回调（通过then设置上去的）
+                    finale();
+                } catch (e) {
+                    reject_(e);
+                }
+            }
 
+            // 作为第二个参数传给你写的那个函数
+            //
+            function reject(newValue) {
+                if (delegating) return;
+                reject_(newValue);
+            }
 
-                    switch (status) {
-                        case 'pending':
-                            //如果上一个promise还处于挂起状态
-                            resolves.push(resolveCallback);
-                            rejects.push(rejectCallback);
-                            break;
-                        case 'fulfilled':
-                            //如果上一个promise得到一个肯定的结果
-                            //resolveCallback(resolve);
-                            resolveCallback(value);
-                            break;
-                        case 'rejected':
-                            rejectCallback(reason);
-                            break;
-                    }
-                })
+            function reject_(newValue) {
+                if (state !== null) return;
+                state = false;
+                value = newValue;
+                finale();
+            }
+
+            // 触发依赖于这个promise的所有回调（通过then设置上去的）
+            function finale() {
+                for (var i = 0, len = deferreds.length; i < len; i++) {
+                    handle(deferreds[i]);
+                }
+                deferreds = null;
+            }
+
+            // 创建完，马上就调用，真直接
+            //
+            try {
+                fn(resolve, reject);
+            } catch (e) {
+                reject(e);
             }
         }
+
+
+        function Handler(onFulfilled, onRejected, resolve, reject) {
+            this.onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null;
+            this.onRejected = typeof onRejected === 'function' ? onRejected : null;
+            this.resolve = resolve;
+            this.reject = reject;
+        }
+
         module.exports = Promise;
     })
 })

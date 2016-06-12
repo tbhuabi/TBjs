22e5ec0158da3740f734d0e1ffb0f2748b3a3a31
18616392776 <464328895@qq.com>
2016-06-12 22:30:34 +0800

    injector

--- classes/src/bootstarp.js
+++ classes/src/bootstarp.js
@@ -1,4 +1,9 @@
 function bootstrap(element, applications) {
-	applications.unshift('TB');
-	createInjector(applications);
+    applications.unshift('TB');
+    var injector = createInjector(applications);
+    injector.invoke(['providerB',
+        function(providerB) {
+
+        }
+    ])
 }
--- classes/src/injector.js
+++ classes/src/injector.js
@@ -15,10 +15,10 @@ function createInjector(applications) {
     forEach(applications, function(appName) {
         appInit(appName);
     })
-	setTimeout(function(){
-		console.log(providerCache)
-		console.log(instanceCache)
-	})
+    setTimeout(function() {
+        console.log(providerCache)
+        console.log(instanceCache)
+    })
 
     function appInit(appName) {
         var app = TBjs.app(appName);
@@ -28,6 +28,9 @@ function createInjector(applications) {
             loadApplication(appName);
         })
         var providerCollection = providerCache[appName] = providerCache[appName] || {};
+        forEach(requires, function(requireAppName) {
+            extend(providerCache[appName], providerCache[requireAppName])
+        })
         forEach(invokeQueue, function(methodRecipe) {
             providerFactory[methodRecipe[0].toLowerCase()](providerCollection, methodRecipe[1], methodRecipe[2], methodRecipe[0]);
         })
@@ -47,25 +50,39 @@ function createInjector(applications) {
         appInit(appName);
     }
 
-    function invoke(provider) {
-        if (isFunction(provider)) {
-            return provider();
-        } else if (isArray(provider)) {
-            var fn = provider.pop();
+    function createInternalInjector(provider, appName) {
+        var $getFn = provider.$get;
+        if (isFunction($getFn)) {
+            return $getFn.call(provider);
+        } else if (isArray($getFn)) {
+            var fn = $getFn.pop();
             var params = provider;
+            var args = [];
             if (!isFunction(fn)) {
                 throw injectorMinErr('invoke', '依赖注入最后一个参数必须为一个函数！');
             }
             forEach(params, function(providerName) {
-				if(!isString(providerName)){
-					throw injectorMinErr('invoke', '依赖注入项必须为字符串！');
-				}
-				//if(instantiate)
+                if (!isString(providerName)) {
+                    throw injectorMinErr('invoke', '依赖注入项必须为字符串！');
+                }
+                if (instanceCache[providerName]) {
+                    args.push(instanceCache[providerName])
+                } else if (providerCache[providerName]) {
+                    var providerInstance = createInternalInjector(providerName, appName);
+                    instanceCache[providerName] = providerInstance;
+                    args.push(providerInstance)
+                } else {
+                    throw injectorMinErr('invoke', '应用 {0} 中，{1}未注册！', appName, providerName);
+                }
             })
+            return fn.apply(provider, args);
         }
         throw injectorMinErr('invoke', 'provider必须提供 `$get` 方法！');
     }
 
+    function invoke(provider, appName) {
+		return createInternalInjector(provider,appName);
+    }
 
     function instantiate() {
 

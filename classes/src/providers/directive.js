2e518048dfbc024378ad5e311c6ee23360845384
18616392776 <464328895@qq.com>
2016-06-16 20:42:16 +0800

    directive

--- classes/src/providers/directive.js
+++ classes/src/providers/directive.js
@@ -2,23 +2,25 @@ function $DirectiveProvider() {
     var directiveSuffix = 'Directive';
     var directiveCache = {};
 
-    this.$get = ['$injector', function($injector) {
-        return {
-            has: hasDirecrtive,
-            get: getDirective
-        }
-
-        function hasDirecrtive(name) {
-            return directiveCache.hasOwnProperty(name) || $injector.has(name + directiveSuffix);
-        }
-
-        function getDirective(name) {
-            if (directiveCache.hasOwnProperty(name)) {
-                return directiveCache[name];
+    this.$get = ['$injector',
+        function($injector) {
+            return {
+                has: hasDirecrtive,
+                get: getDirective
             }
-            return directiveCache[name] = directiveNormalize($injector.get(name + directiveSuffix));
+
+            function hasDirecrtive(name) {
+                return directiveCache.hasOwnProperty(name) || $injector.has(name + directiveSuffix);
+            }
+
+            function getDirective(name) {
+                if (directiveCache.hasOwnProperty(name)) {
+                    return directiveCache[name];
+                }
+                return directiveCache[name] = directiveNormalize($injector.get(name + directiveSuffix));
+            }
         }
-    }];
+    ];
 
 
     function directiveNormalize(directive) {
@@ -31,7 +33,8 @@ function $DirectiveProvider() {
             throw compileMinErr('directive', 'directive必须返回一个控制函数或一个对象!');
         }
         var directiveTemplate = {
-            restrict: 'AM',
+            model: false,
+            restrict: 'EA',
             priority: 1000,
             compile: noop,
             replace: false,
